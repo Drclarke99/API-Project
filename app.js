8690abf79166b25da2df3f05b3c18325d054ab06
David Clarke <drclarke99@gmail.com>
2023-04-28 11:58:26 -0700

    trying to display api data

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // "https://acnhapi.com/v1/fish"
+const fishListEl = document.querySelector('.fish__list');
+
 
 async function main() {
     const drinks = await fetch("https://acnhapi.com/v1/fish");
@@ -8,3 +10,25 @@ async function main() {
 
 main();
 
+async function renderFish() {
+    const fish = await fetch(`https://acnhapi.com/v1/fish/`);
+    const fishData = await fish.json();
+    fishListEl.innerHTML = fishData.map(post => postHTML(post)).join("");
+}
+
+function postHTML(post) {
+    return `
+    <div class="fish">
+      <div class="fish__name">
+        ${post.name}
+      </div>
+      <p class="fish__body">
+        ${post.museum-phrase}
+      </p>
+      <div class="fish__price">
+        Price: $${post.price}
+      </div>
+    </div>`;
+}
+
+renderFish();
