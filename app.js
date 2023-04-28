d78c015a4958892fd19cae02167db7792f9326b1
David Clarke <drclarke99@gmail.com>
2023-04-27 18:37:32 -0700

    made sure api works

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+// "https://acnhapi.com/v1/fish"
 
+async function main() {
+    const drinks = await fetch("https://acnhapi.com/v1/fish");
+    const drinksData = await drinks.json();
+    console.log(drinksData);
+}
 
+main();
 
