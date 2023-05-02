0129d633566e555bd2d1347afdfbc874020baa18
David Clarke <drclarke99@gmail.com>
2023-05-01 19:14:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,33 +5,24 @@ const fishListEl = document.querySelector('.fish__list');
 async function main() {
     const drinks = await fetch("https://acnhapi.com/v1/fish/anchovy");
     const drinksData = await drinks.json();
-    console.log(drinksData.name);
+    console.log(drinksData);
     let dataKeys = Object.entries(drinksData.name);
     let newData = dataKeys.map(key => key[1]);
     console.log(newData[0]);
 }
 
-//main();
+main();
 
 async function renderFish() {
     const fishes = await fetch("https://acnhapi.com/v1/fish");
     const fishData = await fishes.json();
+    console.log(fishData);
     let fishKeys = Object.entries(fishData);
     console.log(fishKeys);
-    let newFishData = fishKeys.map(key => key[1]);
-    console.log(newFishData);
-    let newfishHTML = newFishData.map(user => user.name);
-    console.log(newfishHTML);
-    let newData = newfishHTML.map(name => name[0]);
-    console.log(newData);
     //fishListEl.innerHTML = fishKeys.map(user => userHTML(user)).join("");
 }
 
 renderFish();
-
-function fishHTML(post) {
-    return post[1];
-}
 
 function userHTML(user) {
   /*return `
