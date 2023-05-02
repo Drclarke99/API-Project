28444ff79ea8b971ac5e9d7d422d874fc4f74087
David Clarke <drclarke99@gmail.com>
2023-05-01 18:33:07 -0700

    trying to get correct info from api

--- app.js
+++ app.js
@@ -5,31 +5,47 @@ const fishListEl = document.querySelector('.fish__list');
 async function main() {
     const drinks = await fetch("https://acnhapi.com/v1/fish/anchovy");
     const drinksData = await drinks.json();
-    const fishName = drinksData.name;
-    console.log(fishName);
+    console.log(drinksData.name);
+    let dataKeys = Object.entries(drinksData.name);
+    let newData = dataKeys.map(key => key[1]);
+    console.log(newData[0]);
 }
 
 main();
 
 async function renderFish() {
-    const fish = await fetch(`https://acnhapi.com/v1/fish`);
-    const fishData = await fish.json();
-    fishListEl.innerHTML = fishData.map(post => fishHTML(post)).join("");
-}
-
-function fishHTML(post) {
-    return `
-    <div class="fish">
-      <div class="fish__name">
-        ${post.name-USen}
-      </div>
-      <p class="fish__body">
-        ${post.museum-phrase}
-      </p>
-      <div class="fish__price">
-        Price: $${post.price}
-      </div>
-    </div>`;
+    const fishes = await fetch("https://acnhapi.com/v1/fish");
+    const fishData = await fishes.json();
+    let fishKeys = Object.entries(fishData);
+    console.log(fishKeys);
+    let newFishData = fishKeys.map(key => fishHTML(key));
+    console.log(newFishData);
+    let newfishHTML = fishKeys.map(user => userHTML(user));
+    console.log(newfishHTML);
+    //fishListEl.innerHTML = fishKeys.map(user => userHTML(user)).join("");
 }
 
 renderFish();
+
+function fishHTML(post) {
+    return post[1];
+}
+
+function userHTML(user) {
+  console.log(user[0]);
+
+  return user;
+  /*return `
+    <div class="fish">
+      <div class="fish__name">
+        ${user.name}
+      </div>
+      <p class="fish__body">
+        ${user.museum-phrase}
+      </p>
+      <div class="fish__price">
+        Price: $${user.price}
+      </div>
+    </div>`;*/
+}
+
