5f74306c8bb2342604f70f521bee1d9908ccf7ce
David Clarke <drclarke99@gmail.com>
2023-05-02 19:13:18 -0700

    trying to get filters to work

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 // "https://acnhapi.com/v1/fish"
 const fishListEl = document.querySelector('.fish__list');
 
-async function renderFish() {
+async function displayFish() {
     const fishes = await fetch("https://acnhapi.com/v1/fish");
     const fishData = await fishes.json();
     let fishKeys = Object.entries(fishData);
     fishListEl.innerHTML = fishKeys.map(user => userHTML(user)).join("");
 }
 
-renderFish();
+async function renderFish(filter) {
+  const fishes = await fetch("https://acnhapi.com/v1/fish");
+  const fishData = await fishes.json();
+  let fishKeys = Object.entries(fishData);
+  console.log(fishKeys);
+  let fish;
+  for (let i = 0; i <= fishKeys.length; i++) {
+    fish += fishKeys[i];//.name['name-USen'];
+  }
+  console.log(fish);
+  if (filter === 'Z_TO_A') {
+    fish.sort();
+    fish.reverse();
+  }
+  else if (filter === 'A_TO_Z') {
+    fish.sort();
+  }
+  //console.log(fish);
+  //fishListEl.innerHTML = fishKeys.map(user => userHTML(user)).join("");
+}
 
 function userHTML(user) {
   return `
@@ -25,3 +44,8 @@ function userHTML(user) {
     </div>`;
 }
 
+function filterFish(event) {
+  renderFish(event.target.value);
+}
+
+displayFish();
