887ddf364b99be876cdaf085faf7b99263c4022c
David Clarke <drclarke99@gmail.com>
2023-05-02 17:31:56 -0700

    got api info displayed

--- app.js
+++ app.js
@@ -1,41 +1,27 @@
 // "https://acnhapi.com/v1/fish"
 const fishListEl = document.querySelector('.fish__list');
 
-
-async function main() {
-    const drinks = await fetch("https://acnhapi.com/v1/fish/anchovy");
-    const drinksData = await drinks.json();
-    console.log(drinksData);
-    let dataKeys = Object.entries(drinksData.name);
-    let newData = dataKeys.map(key => key[1]);
-    console.log(newData[0]);
-}
-
-main();
-
 async function renderFish() {
     const fishes = await fetch("https://acnhapi.com/v1/fish");
     const fishData = await fishes.json();
-    console.log(fishData);
     let fishKeys = Object.entries(fishData);
-    console.log(fishKeys);
-    //fishListEl.innerHTML = fishKeys.map(user => userHTML(user)).join("");
+    fishListEl.innerHTML = fishKeys.map(user => userHTML(user)).join("");
 }
 
 renderFish();
 
 function userHTML(user) {
-  /*return `
+  return `
     <div class="fish">
       <div class="fish__name">
-        ${user.name}
+        ${user[1].name['name-USen']}
       </div>
       <p class="fish__body">
-        ${user.museum-phrase}
+        ${user[1]['museum-phrase']}
       </p>
       <div class="fish__price">
-        Price: $${user.price}
+        Price: $${user[1].price}
       </div>
-    </div>`;*/
+    </div>`;
 }
 
